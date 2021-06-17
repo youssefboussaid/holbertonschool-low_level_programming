8890dcbe4ae2a4bf1804e14cb32a40a48d02547b
youssefboussaid <youssbsd@gmail.com>
2021-06-17 04:00:51 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+#include <stdio.h>
+int main()
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+   puts("\"Programming is like building a multilingual puzzle");
+   return 0;
 }
