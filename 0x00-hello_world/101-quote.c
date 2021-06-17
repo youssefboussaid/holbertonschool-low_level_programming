1f0f6f74ffb4912f0cfee1ea4b5ddc0593225ad5
youssefboussaid <youssbsd@gmail.com>
2021-06-17 05:40:40 -0700

    task101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -7,7 +6,6 @@
  */
 int main(void)
 {
-   write(1," and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13);
-
+   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
    return (0);
 }
