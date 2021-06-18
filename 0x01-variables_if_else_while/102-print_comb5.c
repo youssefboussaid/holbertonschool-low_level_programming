bd2240457e3b031ce28da1dd78bf5fe880a74dd3
youssefboussaid <youssbsd@gmail.com>
2021-06-18 06:59:29 -0700

    conbination with 3 numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+for (i = '0' ; i < '9' ; i++)
+{
+	for (j = '1' ; j <= '9' ; j++)
+	{
+	for (k = '1' ; k <= '9' ; k++)
+	{
+	if (i < j && j < k)
+{
+	putchar(i);
+	putchar(j);
+	putchar(k);
+	if (i == '7' && j == '8' && k=='9')
+	{
+	continue;
+	}
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
