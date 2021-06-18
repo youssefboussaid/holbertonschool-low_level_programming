862e94cd60c4f91bad3920c160d1a2a1948f7804
youssefboussaid <youssbsd@gmail.com>
2021-06-18 04:46:51 -0700

    the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char ch;
+for(ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+
+return (0);
+}
