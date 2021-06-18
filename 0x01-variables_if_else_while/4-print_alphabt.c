5801e0b48aaa7e824375e9ad2306654290a7ce5b
youssefboussaid <youssbsd@gmail.com>
2021-06-18 04:58:54 -0700

    alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char ch;
+for(ch = 'a' ; ch <= 'z' ; ch++)
+{
+        if (ch!='e' && ch!='q')
+        putchar(ch);
+}
+return (0);
+}       
