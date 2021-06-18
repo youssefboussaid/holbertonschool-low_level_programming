cd608cd44f5336fc3c2a655b5907698ea94d4b51
youssefboussaid <youssbsd@gmail.com>
2021-06-18 06:47:30 -0700

    combination with 2 numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,10 +10,10 @@ int i;
 int j;
 for (i = '0' ; i < '9' ; i++)
 {
-	for (j = '1' ; j <= '9' ; j++ )
+	for (j = '1' ; j <= '9' ; j++)
 	{
-	if  (i<j)
-        {
+	if (i < j)
+{
 	putchar(i);
 	putchar(j);
 	if (i == '8' && j == '9')
