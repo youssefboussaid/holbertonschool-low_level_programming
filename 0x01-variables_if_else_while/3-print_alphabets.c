3f6e928917131a85311e94ae93f75c98df53229a
youssefboussaid <youssbsd@gmail.com>
2021-06-18 04:53:49 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+        putchar(ch);
+}
+for (ch = 'A' ; ch<= 'Z' ; ch++)
+{
+ 	putchar(ch);
+}
+return (0)
+}
