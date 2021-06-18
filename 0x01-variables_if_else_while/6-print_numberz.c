bd950acefac691ab76f7890b303b4505beb3ca11
youssefboussaid <youssbsd@gmail.com>
2021-06-18 05:20:58 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = '0' ; a <= '9' ; x++)
+{
+	putchar(%d, x);
+}
+	putchar('\n');
+return (0);
+}
