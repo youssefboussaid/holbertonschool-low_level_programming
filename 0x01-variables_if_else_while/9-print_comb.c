433d4976a74e212999d3d9f8f04f9b09aaad9c0c
youssefboussaid <youssbsd@gmail.com>
2021-06-18 05:46:37 -0700

    number combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+	putchar((x % 10) + '0');
+}
+putchar(',');
+putchar(' ');
+return (0);
+}
