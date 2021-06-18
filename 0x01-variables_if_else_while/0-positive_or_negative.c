c161e83d2acf0d063d7eb8c64b5c34ea1988f4cc
youssefboussaid <youssbsd@gmail.com>
2021-06-18 04:08:11 -0700

    positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n>0)
+	printf("n is positive");
+else if (n=0)
+	printf("n is zero");
+else if (n<0)
+	printf("n is negative");
+return (0);
+}
