2f4c4b7d6883dd664eced9dba350378761fca599
youssefboussaid <youssbsd@gmail.com>
2021-06-18 05:52:07 -0700

    number combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,11 @@ int main(void)
 int x;
 for (x = 0; x < 10; x++)
 {
-putchar((x % 10) + '0');
-putchar(',');
-putchar(' ');
+	putchar((x % 10) + '0');
+if (x == 9)
+	continue;
+	putchar(',');
+	putchar(' ');
 }
 return (0);
 }
