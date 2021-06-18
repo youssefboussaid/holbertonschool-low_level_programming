120209c2ba7c1153b92d9c30deb29ed75a5bb574
youssefboussaid <youssbsd@gmail.com>
2021-06-18 06:44:37 -0700

    combination with 2 numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+int j;
+for (i='0' ; i < '9' ; i++)
+{
+    for (j='1' ; j <= '9' ; j++ )
+    {
+        if  (i<j)
+        {
+    putchar(i);
+    putchar(j);
+    if (i=='8' && j=='9')
+    {
+    continue;
+    }
+    
+        
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
