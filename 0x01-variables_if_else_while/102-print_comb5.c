0c552632a84821ed2fe676ad7a126b2fd7847757
youssefboussaid <youssbsd@gmail.com>
2021-06-18 07:05:40 -0700

    conbination with 3 numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
+
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i;
-int j;
-int k;
-for (i = '0' ; i < '9' ; i++)
-{
-	for (j = '1' ; j <= '9' ; j++)
+	int a, b, c, d;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	while (a * 10 + b < 100)
 	{
-	for (k = '1' ; k <= '9' ; k++)
-	{
-	if (i < j && j < k)
-{
-	putchar(i);
-	putchar(j);
-	putchar(k);
-	if (i == '7' && j == '8' && k=='9')
-	{
-	continue;
+		if (c * 10 + d > a * 10 + b)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(' ');
+			putchar(c + '0');
+			putchar(d + '0');
+			if (a * 10 + b < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		d++;
+		if (d > 9)
+		{
+			c++;
+			d = 0;
+		}
+		if (c > 9)
+		{
+			b++;
+			c = 0;
+		}
+		if (b > 9)
+		{
+			a++;
+			b = 0;
+		}
 	}
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
