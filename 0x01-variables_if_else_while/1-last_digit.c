ffa76179927969de3c81d6df9135304df5c1fa36
youssefboussaid <youssbsd@gmail.com>
2021-06-18 04:42:44 -0700

    the alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,8 +17,8 @@ lastdigit = n % 10;
 if (lastdigit > 5)
 	printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 else if (lastdigit == 0)
-       printf("Last digit of %d is %d and is 0\n", n, lastdigit);
-else if (lastdigit < 5)
+	printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+if( lastdigit < 5)
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 return (0);
 }
