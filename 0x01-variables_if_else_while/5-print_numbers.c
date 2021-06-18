18bb0e44fac16a524831cf7470c767673dab7b2a
youssefboussaid <youssbsd@gmail.com>
2021-06-18 05:04:24 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;	
+for (n=1 ;n < 10 ;n++)
+printf("%d", n);
+return (0);
+}
