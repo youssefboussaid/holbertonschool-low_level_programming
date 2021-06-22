31249f286119c9f781b65ae7669f3592f6fa41f1
youssefboussaid <youssbsd@gmail.com>
2021-06-22 12:44:03 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch[100] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(ch[i]);
+	_putchar('\n');
+	return (0);
+}
