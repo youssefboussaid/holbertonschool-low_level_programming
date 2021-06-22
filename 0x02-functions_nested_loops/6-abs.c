0dcb91adb9163177bbbe2e49ba55e09421a29477
youssefboussaid <youssbsd@gmail.com>
2021-06-22 12:50:07 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		n = n * (-1);
+		return (n);
+	}
+}
