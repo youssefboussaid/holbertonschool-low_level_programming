f6e6c7dc77eaf326c694664c64a7244edaeaee3d
youssefboussaid <youssbsd@gmail.com>
2021-06-28 05:12:12 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: The array of integers to be reversed.
+ * @n: The number of elements in the array.
+ */
+
+void print_rev(char *s)
+{
+	while(*s)
+		*s++;
+	while(*s)
+		_putchar(*str++);
+}
