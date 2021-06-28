440cd703ae88b2dd1f3e4305cd2b08e78fa038af
youssefboussaid <youssbsd@gmail.com>
2021-06-28 03:20:02 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * strlen - Write a function that returns the length of a string.
+ *
+ * @s :pointer of a char
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while(*s[c] != '\0')
+		c++;
+	return c;
+}
