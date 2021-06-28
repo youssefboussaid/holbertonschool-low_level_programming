237d032660d234069fbe88876b80db80255f332d
youssefboussaid <youssbsd@gmail.com>
2021-06-28 03:00:51 -0700

    task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * swap_int - Write a function that swaps the values of two integers.    
- * @a: The pointer to an int.
- * @b: The pointer to an int.
+ * swap_int - Write a function that swaps the values of two integers.
+ *
+ * @a :pointer to an int
+ * @b : pointer to an int
  */
 void swap_int(int *a, int *b)
 {
