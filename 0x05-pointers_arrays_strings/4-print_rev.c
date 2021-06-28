a6df0315c2eea6e1b61f66b2e8e9ad9627d669c4
youssefboussaid <youssbsd@gmail.com>
2021-06-28 05:33:24 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,11 +5,12 @@
  * @a: The array of integers to be reversed.
  * @n: The number of elements in the array.
  */
-
 void print_rev(char *s)
 {
-	while(*s)
-		s++;
-	while(*s)
-		_putchar(s--);
+	int c;
+	int len = _strlen(s);
+
+	for (c=len ; c>=0 ;c--)
+		_putchar(s[c]);
+	_putchar('\n');
 }
