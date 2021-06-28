d03bb4a0aff4b0104c866990cf5351c02dbc5b55
youssefboussaid <youssbsd@gmail.com>
2021-06-28 02:34:02 -0700

    task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * reset_to_98 - Write a function that takes a pointer to an int 
- *              as parameter and updates the value it points to to 98.
+ * as parameter and updates the value it points to to 98.
  * @n: pointer to an int
  */
 void reset_to_98(int *n)
