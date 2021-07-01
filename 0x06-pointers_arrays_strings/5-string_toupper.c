8af20b8c9259401dd8bd6cc998144e91f1f1a638
youssefboussaid <youssbsd@gmail.com>
2021-07-01 04:36:18 -0700

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,13 @@
  * Return: A pointer to the changed string.
  */
 
-char *string_toupper(char *str);
+char *string_toupper(char *str)
 {
 	int i = 0;
 
 	while (str[i])
 	{
-		if (str[i] > 'a' && str[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - 32;
 	i++;	
 	}
