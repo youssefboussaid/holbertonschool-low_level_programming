d24c1f572e61827f90d8011e882afae50d547975
youssefboussaid <youssbsd@gmail.com>
2021-07-01 03:13:57 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 
 /**
- * _strcat - Write a function that concatenates two strings.
+ * strcat - Concatenates the string pointed to by @src, including the terminating
+ *          null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
  *
- * @src :pointer to a cher
- * @dest : pointer to a char
- * Return: dest+src
+ * Return: A pointer to the destination string @dest.
  */
-
 char *strcat(char *dest, const char *src)
 {
-	int i = 0, dest_len = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[i++])
+	while (dest[index++])
 		dest_len++;
 
-	for (i = 0; src[i]; i++)
-		dest[dest_len++] = src[i];
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
