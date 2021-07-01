3283f74df6df4c0990ebc5cf517e91194c81eb00
youssefboussaid <youssbsd@gmail.com>
2021-07-01 02:57:09 -0700

    task7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * leet - Write a function that encodes a string into 1337.
+ * @str: The string to be reversed.
+ * Return: a pointer to the encoded string
+ */
+
+char *leet(char *str)
+{
+	int i=0;
+	int j;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[i] != '\0')
+	{
+		for ( j = 0 ; j <=7; j++)
+		{
+		      if (str[i] == lett[j]
+				|| str[i] - 32 == lett[j])
+				str[i] = j;
+		}
+		i++;
+	}
+	return(str);
+}
