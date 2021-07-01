1652d84e454cf2cfab869aad95f5766a324f32c6
youssefboussaid <youssbsd@gmail.com>
2021-07-01 06:15:15 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,19 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int f = 0;
-
-	while (*s1 != '\0' || *s2 != '\0')
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		if (*s1 != *s2)
-		{
-			f = 1;
-			break;
-		}
-	s1++;
-	s2++;
+		s1++;
+		s2++;
 	}
-if (f == 0)
-	return (0);
-else
-	return (-1);
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*strg1 - *strg2);
+	}
 }
