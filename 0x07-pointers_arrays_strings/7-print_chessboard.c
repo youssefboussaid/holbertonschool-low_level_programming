0b0064fa214b805626d14ad588ff65f8b597efb6
youssefboussaid <youssbsd@gmail.com>
2021-07-05 13:08:25 -0700

    task7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,16 @@
-#include "bootcamp.h"
+#include "holberton.h"
 /**
- * print_chessboard - prints 2D array of chessboard
- * @a: chessboard array
- * Return: void
+ * print_chessboard - prints the chessboard
+ * @a: array to print
  */
 void print_chessboard(char (*a)[8])
 {
-	int j = 1;
+	int i, j;
 
-	while (a[0][j - 1] != 0)
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(a[0][j - 1]);
-		if (j > 1 && j % 8 == 0)
-			_putchar('\n');
-		j++;
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
 	}
 }
