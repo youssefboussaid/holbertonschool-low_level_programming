125a76544d6c662581f5ad4f38aabbdd0c4afef9
youssefboussaid <youssbsd@gmail.com>
2021-07-05 04:01:43 -0700

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+  * _memcpy - copies n characters from memory area src to memory area dest.
+  * @dest: copies n characters from memory area src to memory area dest.
+  * @src: This is pointer to the source of data to be copied
+  * @n: This is the number of bytes to be copied
+  * Return: This function returns a pointer to destination
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+		dest[i] = src[i];
+	return (dest);
+}
