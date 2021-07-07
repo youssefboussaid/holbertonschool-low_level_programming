5e8485c633fccebbaad1f214c256931781ca4d0b
youssefboussaid <youssbsd@gmail.com>
2021-07-07 02:48:20 -0700

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *_strlen_recursion -   prints the length of the string
+ *@s: pointer to the string 
+ *Return: the length if the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s =='\0')
+		return (0);
+	else 
+		return (1 + _strlen_recursion(s + 1);
+}
