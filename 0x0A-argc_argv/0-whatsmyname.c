a6b8a9f298fb7fd341453a0a0a1fb1990a21073d
youssefboussaid <youssbsd@gmail.com>
2021-07-09 02:04:45 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * main - print the program name
  * @argc : the number of argument
@@ -9,6 +9,6 @@
 
 int main(int argc, char *argv[])
 {
-	__putchar("%s/n", argv[0]);
+	printf("%s/n", argv[0]);
 	return (0);
 }
