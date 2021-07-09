746dfa194061c5df47030c4048018484ddb30bbd
youssefboussaid <youssbsd@gmail.com>
2021-07-09 01:32:01 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* main - prints the number of argument
+* @argc: the number of argument
+* @argv: an arry of the argument
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	_putchar("%d/n", argc);
+	return (0);
+}
