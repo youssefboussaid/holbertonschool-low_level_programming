81634e08817c511261db91fc1dfea6d293db0bc6
youssefboussaid <youssbsd@gmail.com>
2021-07-09 01:27:51 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - print the program name
+ * @argc : the number of argument
+ * @argv : an arry of the argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	__putchar("%s/n", argv[0]);
+	return (0);
+}
