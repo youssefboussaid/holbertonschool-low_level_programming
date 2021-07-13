184ab83a9fb89dd38c1baae6de99326930faaae5
youssefboussaid <youssbsd@gmail.com>
2021-07-13 02:00:37 -0700

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 		return (NULL);
 	for (i = 0; str[i]; i++)
 		len++;
-	p = malloc((sizeof(char) * (len + 1));
+	p = malloc((sizeof(char)) * (len + 1));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; str[i]; i++)
