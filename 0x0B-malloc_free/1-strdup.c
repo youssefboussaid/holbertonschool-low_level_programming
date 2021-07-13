2fe3bb50927d7431ca037a22affda44cd1131bf7
youssefboussaid <youssbsd@gmail.com>
2021-07-13 01:58:41 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicate a string
+ *
+ * @str: the string to be duplicated
+ *
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		len++;
+	p = malloc((sizeof(char) * (len + 1));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		p[i] == str[i];
+	p[len] = '\0';
+	return (p);
+}
