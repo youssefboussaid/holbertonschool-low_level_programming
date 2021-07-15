60bedf2728c54b26da1a6d466cde7f13ef82a328
youssefboussaid <youssbsd@gmail.com>
2021-07-15 04:22:22 -0700

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc 
+ * @b : number of bytes to be reserved
+ *
+ * Return: return a pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
