277c49b7486b13713025a235a4aad59738775717
youssefboussaid <youssbsd@gmail.com>
2021-07-15 06:57:15 -0700

    lib

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -12,5 +12,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #endif
