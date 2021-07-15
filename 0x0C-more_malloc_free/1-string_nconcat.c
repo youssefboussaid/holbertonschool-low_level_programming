286ee1d3bf9da05c50208eb153a2aa90fedbcc73
youssefboussaid <youssbsd@gmail.com>
2021-07-15 05:12:38 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,9 +37,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
+	{
 		p[i] = s1[i];
+	}
 	for (j = 0; j < n; j++)
+	{
 		p[i++] = s2[j];
+	}
 	p[i++] = '\0';
 	return (p);
 }
