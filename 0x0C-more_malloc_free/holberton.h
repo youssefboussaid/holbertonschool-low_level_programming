8c4a22867c3beaf866da56440ec02282edddd0cb
youssefboussaid <youssbsd@gmail.com>
2021-07-15 05:25:23 -0700

    holberton

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,11 +1,11 @@
-#ifndef HEADERFILE_H
-#define HEADERFILE_H
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-#endif
+
+#endif /* _HOLBERTON_H_ */
