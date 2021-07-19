6b83d58cf49757ac23475bb2f0fdf078db920709
youssefboussaid <youssbsd@gmail.com>
2021-07-19 04:55:48 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the name of the owner
+ *
+ * Description: dog struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
