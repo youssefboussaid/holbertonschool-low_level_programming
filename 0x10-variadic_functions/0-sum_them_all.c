19fa45ef57686ec1fe9d355b052789750a8368fe
youssefboussaid <youssbsd@gmail.com>
2021-07-22 10:51:06 -0700

    Task0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 
 /**
  * sum_them_all - returns the sum of all its parameters
- * @n: number of arguments
+ * @n: number of argument
  * Return: returns the sum of all its parameters
  */
 int sum_them_all(const unsigned int n, ...)
