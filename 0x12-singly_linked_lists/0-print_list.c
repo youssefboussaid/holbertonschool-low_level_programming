81cddc659fcc5f47915367b5387ac4e9c9f53ac9
youssefboussaid <youssbsd@gmail.com>
2021-07-29 07:39:34 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		if (h->str != NULL)
 		printf("[%d] %s\n", h->len, h->str);
 		else
-		printf("[0](nill)\n");
+		printf("[0] (nil)\n");
 		n++;
 		h = h->next;
 	}
