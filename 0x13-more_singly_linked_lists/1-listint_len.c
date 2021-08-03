3bacfa16f92387db7b21e2dc3dbd91fd00ef580d
youssefboussaid <youssbsd@gmail.com>
2021-08-03 00:54:35 -0700

    task1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements in a linked list_t list
+ * @h: a variable pointing to a struct of type list_t
+ *
+ * Return: number of elements in linked list
+ */
+
+size_t listint_len(const list_t *h)
+{
+	int n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
