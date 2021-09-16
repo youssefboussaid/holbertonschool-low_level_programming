7f82ffda52eb6d3a2e59626966f3ddbf491c1310
youssefboussaid <youssbsd@gmail.com>
2021-09-16 04:10:25 -0700

    task1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len  - number of elements in a linked dlistint_t
+ * @h: pointer to a struct of type dlistint_t
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int n = 0;
+	
+	while(h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
