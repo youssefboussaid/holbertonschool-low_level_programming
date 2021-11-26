a2842878b80b601adfc9e317db4ab13955f718c6
youssefboussaid <youssbsd@gmail.com>
2021-11-25 19:33:23 -1100

    task5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """find the island perimeter"""
+
+
 def island_perimeter(grid):
     ans = 0
-    for i in range (len(grid)):
-        for j in range (len(grid[0])):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 ans += 4
-                if grid[i-1][j] == 1:
+                if grid[i - 1][j] == 1:
                     ans -= 2
-                if grid[i][j-1] == 1:
+                if grid[i][j - 1] == 1:
                     ans -= 2
-    return ans 
+    return ans
