413a7880c14ede990ac1636fcee6ca00520e941d
youssefboussaid <youssbsd@gmail.com>
2022-01-18 11:55:56 -1100

    lib

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALROS_H
+#define SEARCH_ALROS_H
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int _putchar(char c);
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+
+#endif
